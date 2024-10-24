071de279867ef5d1facba3efe92c89493b06e07f
rochmadnf <rochmadnf@gmail.com>
2024-10-25 02:57:42 +0800

    change view

--- resources/views/pages/researcher.blade.php
+++ resources/views/pages/researcher.blade.php
@@ -9,14 +9,13 @@
                 <div class="flex h-[540px] w-1/2 flex-col items-center justify-between gap-y-10 rounded-xl bg-rose-100 py-6">
                     <x-logo />
 
-                    <img src="{{ asset('assets/illustration/asset-6.png') }}" class="w-72" alt="Map">
+                    <img src="{{ asset('assets/illustration/asset-1.png') }}" class="w-72" alt="Map">
 
                     <h1 class="select-none border-b-4 border-b-gray-900 text-3xl font-bold uppercase tracking-wide">Peneliti
                     </h1>
                 </div>
 
-                <div class="h-[540px] w-1/2 overflow-y-auto rounded-xl border border-slate-200 px-6 py-4">
-
+                <div class="h-[540px] w-1/2 overflow-y-auto rounded-xl border border-slate-200 px-8 pb-4 pt-8">
                     @foreach ($researchers as $researcher)
                         <div
                             class="mb-4 flex flex-col items-center justify-center rounded-md bg-slate-50/30 p-4 shadow-0 shadow-slate-300">
